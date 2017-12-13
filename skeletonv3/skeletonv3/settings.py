39468761e065b3feafd995dccb2a846c452fe832
Kevin Barbour <kevbarbour@gmail.com>
2017-12-12 18:49:17 -0700

    updated settings2

--- skeletonv3/skeletonv3/settings.py
+++ skeletonv3/skeletonv3/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!@v^hnq4+-lw5qscs@4guhjv__!h-k#m(0mh24@b(zd(1+*zuf'
+# SECRET_KEY = '!@v^hnq4+-lw5qscs@4guhjv__!h-k#m(0mh24@b(zd(1+*zuf'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!@v^hnq4+-lw5qscs@4guhjv__!h-k#m(0mh24@b(zd(1+*zuf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
